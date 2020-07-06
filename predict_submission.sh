e201475f88720a1f46a58e52ab53fe14a8e8e30f
Selim Seferbekov <selim.sef@gmail.com>
2020-07-06 23:42:39 +0300

    added missing checkpoint

--- predict_submission.sh
+++ predict_submission.sh
@@ -6,6 +6,7 @@ python predict_folder.py \
  --output $CSV \
  --models final_111_DeepFakeClassifier_tf_efficientnet_b7_ns_0_36 \
   final_555_DeepFakeClassifier_tf_efficientnet_b7_ns_0_19 \
+  final_777_DeepFakeClassifier_tf_efficientnet_b7_ns_0_29 \
   final_777_DeepFakeClassifier_tf_efficientnet_b7_ns_0_31 \
   final_888_DeepFakeClassifier_tf_efficientnet_b7_ns_0_37 \
   final_888_DeepFakeClassifier_tf_efficientnet_b7_ns_0_40 \
