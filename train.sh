ee2cb756379ae352e9ea284f35eef64695d26e54
Saeyoon Oh <saeyoon17@kaist.ac.kr>
2023-02-10 03:41:20 +0900

    vessl try logging only validations

--- train.sh
+++ train.sh
@@ -2,17 +2,17 @@
 
 ROOT_DIR='/home/coinse/greentea/src/dfdc_deepfake_challenge/data_root'
 NUM_GPUS='2'
-OPT=$1
-BATCH_SIZE=$2
-LR=$3
-WD=$4
+# OPT=$1
+# BATCH_SIZE=$2
+# LR=$3
+# WD=$4
 
-cat configs/b7.json | jq .optimizer.type=$OPT
-cat configs/b7.json | jq .optimizer.batch_size=$BATCH_SIZE
-cat configs/b7.json | jq .optimizer.learning_rate=$LR
-cat configs/b7.json | jq .optimizer.weight_decay=$WD
+# cat configs/b7.json | jq .optimizer.type=$OPT
+# cat configs/b7.json | jq .optimizer.batch_size=$BATCH_SIZE
+# cat configs/b7.json | jq .optimizer.learning_rate=$LR
+# cat configs/b7.json | jq .optimizer.weight_decay=$WD
 
-python -u -m torch.distributed.launch --nproc_per_node=$NUM_GPUS --master_port 9901 training/pipelines/train_classifier.py \
+python -u -m torch.distributed.launch --nproc_per_node=$NUM_GPUS --master_port 9902 training/pipelines/train_classifier.py \
  --distributed --config configs/b7.json --freeze-epochs 0 --test_every 1 --opt-level O1 --label-smoothing 0.01 --folds-csv folds.csv   --fold 0 --seed 111 --data-dir $ROOT_DIR --prefix b7_111_ > logs/b7_111
 
 # python -u -m torch.distributed.launch --nproc_per_node=$NUM_GPUS --master_port 9901 training/pipelines/train_classifier.py \
